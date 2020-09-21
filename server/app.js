b37f7206d31fcb602bca8c39d805490f71dff4e5
Andre Veremenko <veremenko.andre@gmail.com>
2020-09-21 20:54:44 +0300

    реализовал возвратак инлекса при любом адрессе создал файл роутинга

--- server/app.js
+++ server/app.js
@@ -8,7 +8,14 @@ const app = express();
 const PORT = config.port;
 const publicPath = path.join(__dirname,config.public_path);
 
+app.use(express.json());
 app.use(express.static(publicPath));
+
+app.use('/api',require('./router'));
+app.use('*',(req,res)=>{
+    const file = path.join(publicPath,'index.html');
+    res.sendFile(file);
+});
 
 app.listen(PORT,()=>{
     console.log(`server listen ${PORT}`);
